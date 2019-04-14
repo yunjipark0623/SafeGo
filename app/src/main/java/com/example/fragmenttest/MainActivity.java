a84a974f1fc2dcf8759d006ca61892cad2e0e198
YoonSeokHan <31535194+YoonSeokHan@users.noreply.github.com>
2019-04-14 20:53:23 +0900

    두번째

--- app/src/main/java/com/example/fragmenttest/MainActivity.java
+++ app/src/main/java/com/example/fragmenttest/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.fragmenttest;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.fragmenttest.Fragment.MondayFragment;
+import com.example.fragmenttest.Fragment.TueFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    PagerAdapter pagerAdapter;
+    TabLayout tl;
+    ViewPager vp;
+    FragmentTransaction ft;
+    FragmentManager fm;
+    Fragment fragment;
+    //int count=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tl=findViewById(R.id.tabLayout);
+        pagerAdapter=new PagerAdapter(getSupportFragmentManager());
+        vp=(ViewPager)findViewById(R.id.viewpager);
+        vp.setAdapter(pagerAdapter);
+        vp.setOnClickListener(this);
+
     }
 }
+
