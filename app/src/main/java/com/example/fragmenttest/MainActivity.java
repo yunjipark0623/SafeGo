7dc819cdbbed37bcab5b32d19c42ff29e8433f21
YoonSeokHan <31535194+YoonSeokHan@users.noreply.github.com>
2019-04-16 00:45:09 +0900

    프레그먼트기본완성

--- app/src/main/java/com/example/fragmenttest/MainActivity.java
+++ app/src/main/java/com/example/fragmenttest/MainActivity.java
@@ -1,38 +1,71 @@
 package com.example.fragmenttest;
 
+import android.content.Context;
+import android.net.Uri;
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 
-import com.example.fragmenttest.Fragment.MondayFragment;
-import com.example.fragmenttest.Fragment.TueFragment;
+import com.example.fragmenttest.Fragment.HowTo;
 
-public class MainActivity extends AppCompatActivity {
 
-    PagerAdapter pagerAdapter;
-    TabLayout tl;
-    ViewPager vp;
-    FragmentTransaction ft;
-    FragmentManager fm;
-    Fragment fragment;
-    //int count=0;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        tl=findViewById(R.id.tabLayout);
-        pagerAdapter=new PagerAdapter(getSupportFragmentManager());
-        vp=(ViewPager)findViewById(R.id.viewpager);
-        vp.setAdapter(pagerAdapter);
-        vp.setOnClickListener(this);
+public class MainActivity extends AppCompatActivity{
 
+        private Context mContext;
+        private TabLayout mTabLayout;
+
+        private ViewPager mViewPager;
+        private ContentsPagerAdapter mContentsPagerAdapter;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            mContext = getApplicationContext();
+
+            mTabLayout = (TabLayout) findViewById(R.id.tabLayout);
+
+            mTabLayout.addTab(mTabLayout.newTab().setCustomView(createTabView("메뉴얼")));
+            mTabLayout.addTab(mTabLayout.newTab().setCustomView(createTabView("속도")));
+            mTabLayout.addTab(mTabLayout.newTab().setCustomView(createTabView("경적")));
+            mTabLayout.addTab(mTabLayout.newTab().setCustomView(createTabView("커뮤니티")));
+            mTabLayout.addTab(mTabLayout.newTab().setCustomView(createTabView("프로필")));
+//
+            mViewPager = (ViewPager) findViewById(R.id.pager_content);
+            mContentsPagerAdapter = new ContentsPagerAdapter(getSupportFragmentManager(), mTabLayout.getTabCount());
+
+            mViewPager.setAdapter(mContentsPagerAdapter);
+
+            mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTabLayout));
+
+            mTabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+                @Override
+                public void onTabSelected(TabLayout.Tab tab) {
+                    mViewPager.setCurrentItem(tab.getPosition());
+                }
+
+                @Override
+                public void onTabUnselected(TabLayout.Tab tab) {
+
+                }
+
+                @Override
+                public void onTabReselected(TabLayout.Tab tab) {
+
+                }
+            });
+        }
+            private View createTabView(String tabName){
+                View tabView = LayoutInflater.from(mContext).inflate(R.layout.custom_tab,null);
+                TextView txt_name = (TextView) tabView.findViewById(R.id.txt_name);
+                txt_name.setText(tabName);
+                return tabView;
+        }
     }
-}
+
 
